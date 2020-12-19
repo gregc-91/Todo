a3ebc97300994c6f19ea5f6c9d31933673046a88
Greg <41692114+gregc-91@users.noreply.github.com>
2020-12-19 10:44:44 +0000

    Adding parsing of add command, no execution yet

--- src/main.cpp
+++ src/main.cpp
@@ -73,7 +73,7 @@ Command parseCommandType(char* str) {
 
 void parseListCommand(int argc, char** argv, Command &command) {
 	
-	for (unsigned i = 0; i < argc; i++) {
+	for (int i = 0; i < argc; i++) {
 		
 		switch (argv[i][0]) {
 		
@@ -104,13 +104,41 @@ void parseListCommand(int argc, char** argv, Command &command) {
 	}
 }
 
+void parseAddCommand(int argc, char** argv, Command &command) {
+	
+	for (int i = 0; i < argc; i++) {
+		
+		switch (argv[i][0]) {
+			
+		case '#':
+			if (argv[i][1] == '\0') throw std::runtime_error("Missing project name");
+			command.add.project = std::string(argv[i]+1);
+			break;
+		case '@':
+			if (argv[i][1] == '\0') throw std::runtime_error("Missing tag name");
+			command.add.tag = std::string(argv[i]+1);
+			break;
+		default:
+			if (!command.add.task.empty()) throw std::runtime_error("Duplicate task name");
+			command.add.task = std::string(argv[i]);
+			break;
+		}
+	}
+	
+	if (command.add.task.empty())
+		throw std::runtime_error("Missing line to add");
+}
+
 Command parseCommand(int argc, char** argv) {
 	if (argc < 2)  throw std::invalid_argument("Not enough arguments!");
 	
 	Command command = parseCommandType(argv[1]);
 	
-	if (argc > 2 && command.type() == CommandType::List) {
+	if (command.type() == CommandType::List) {
 		parseListCommand(argc-2, argv+2, command);
+	}
+	if (command.type() == CommandType::Add) {
+		parseAddCommand(argc-2, argv+2, command);
 	}
 
 	return command;
